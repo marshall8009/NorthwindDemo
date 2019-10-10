8e9540a26a51bcd7f4545ea4c604c86d47f492c6
Jake <marshall_8009@yahoo.com>
2019-10-10 11:34:09 -0700

    adding product model, data connection, controller and view

---
+++ NorthwindLibrary/Models/ProductModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthwindLibrary.Models
+{
+    public class ProductModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierID { get; set; }
+        public int CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public ushort UnitsInStock { get; set; }
+        public ushort UnitsOnOrder { get; set; }
+        public ushort ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}
